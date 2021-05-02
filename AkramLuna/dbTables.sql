aa90368d19f0c21f4396061e5a4b349ed8ea11c4
montielantonio <ggtonushy@gmail.com>
2021-05-02 20:04:14 +0200

    upd

--- AkramLuna/dbTables.sql
+++ AkramLuna/dbTables.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.3.24-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.0.0.6104
+-- Server Version:               10.3.28-MariaDB - mariadb.org binary distribution
+-- Server Betriebssystem:        Win64
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,11 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for stockdb_akramluna
+-- Exportiere Datenbank Struktur für stockdb_akramluna
 CREATE DATABASE IF NOT EXISTS `stockdb_akramluna` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `stockdb_akramluna`;
 
--- Dumping structure for table stockdb_akramluna.stockdailydata
+-- Exportiere Struktur von Tabelle stockdb_akramluna.stockdailydata
 CREATE TABLE IF NOT EXISTS `stockdailydata` (
   `ticker` varchar(50) NOT NULL,
   `dateMeasure` date NOT NULL,
@@ -30,81 +30,57 @@ CREATE TABLE IF NOT EXISTS `stockdailydata` (
   PRIMARY KEY (`ticker`,`dateMeasure`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Daten Export vom Benutzer nicht ausgewählt
 
--- Dumping structure for table stockdb_akramluna.stockfinancials
+-- Exportiere Struktur von Tabelle stockdb_akramluna.stockfinancials
 CREATE TABLE IF NOT EXISTS `stockfinancials` (
   `ticker` varchar(50) NOT NULL,
   `dateCal` date NOT NULL,
   `dateReport` date DEFAULT NULL,
   `price` float DEFAULT NULL,
-  `netinc` int(11) DEFAULT NULL,
-  `marketcap` int(11) DEFAULT NULL,
-  `accoci` int(11) DEFAULT NULL,
-  `assets` int(11) DEFAULT NULL,
-  `assetsavg` int(11) DEFAULT NULL,
-  `assetturnover` float DEFAULT NULL,
-  `bvps` float DEFAULT NULL,
-  `capex` int(11) DEFAULT NULL,
-  `cashneq` int(11) DEFAULT NULL,
-  `cor` int(11) DEFAULT NULL,
+  `netinc` bigint(20) DEFAULT NULL,
+  `marketcap` bigint(20) DEFAULT NULL,
+  `assets` bigint(20) DEFAULT NULL,
+  `capex` bigint(20) DEFAULT NULL,
+  `cashneq` bigint(20) DEFAULT NULL,
   `currentratio` float DEFAULT NULL,
-  `de` float DEFAULT NULL,
-  `debt` int(11) DEFAULT NULL,
-  `deferredrev` int(11) DEFAULT NULL,
-  `depamor` int(11) DEFAULT NULL,
-  `ebit` int(11) DEFAULT NULL,
-  `ebitda` int(11) DEFAULT NULL,
+  `debt` bigint(20) DEFAULT NULL,
+  `ebit` bigint(20) DEFAULT NULL,
+  `ebitda` bigint(20) DEFAULT NULL,
   `ebitdamargin` float DEFAULT NULL,
   `eps` float DEFAULT NULL,
-  `equity` int(11) DEFAULT NULL,
-  `ev` int(11) DEFAULT NULL,
+  `equity` bigint(20) DEFAULT NULL,
+  `ev` bigint(20) DEFAULT NULL,
   `evebit` float DEFAULT NULL,
   `evebidta` float DEFAULT NULL,
-  `fcf` int(11) DEFAULT NULL,
+  `fcf` bigint(20) DEFAULT NULL,
   `fcfps` float DEFAULT NULL,
-  `gp` int(11) DEFAULT NULL,
+  `gp` bigint(20) DEFAULT NULL,
   `grossmargin` float DEFAULT NULL,
-  `intangibles` int(11) DEFAULT NULL,
-  `invcap` int(11) DEFAULT NULL,
-  `inventory` int(11) DEFAULT NULL,
-  `investments` int(11) DEFAULT NULL,
-  `liabilities` int(11) DEFAULT NULL,
-  `ncf` int(11) DEFAULT NULL,
-  `ncfbus` int(11) DEFAULT NULL,
-  `ncfcommon` int(11) DEFAULT NULL,
+  `inventory` bigint(20) DEFAULT NULL,
+  `investments` bigint(20) DEFAULT NULL,
+  `liabilities` bigint(20) DEFAULT NULL,
+  `ncf` bigint(20) DEFAULT NULL,
   `netmargin` float DEFAULT NULL,
-  `opex` int(11) DEFAULT NULL,
-  `opinc` int(11) DEFAULT NULL,
-  `payables` int(11) DEFAULT NULL,
+  `payables` bigint(20) DEFAULT NULL,
   `pb` float DEFAULT NULL,
   `pe` float DEFAULT NULL,
   `ps` float DEFAULT NULL,
-  `receivables` int(11) DEFAULT NULL,
-  `revenue` int(11) DEFAULT NULL,
-  `rnd` int(11) DEFAULT NULL,
+  `receivables` bigint(20) DEFAULT NULL,
+  `revenue` bigint(20) DEFAULT NULL,
+  `rnd` bigint(20) DEFAULT NULL,
   `roa` float DEFAULT NULL,
   `roe` float DEFAULT NULL,
   `roic` float DEFAULT NULL,
   `ros` float DEFAULT NULL,
-  `sbcomp` int(11) DEFAULT NULL,
-  `sgna` int(11) DEFAULT NULL,
-  `sharesbas` int(11) DEFAULT NULL,
-  `shareswa` int(11) DEFAULT NULL,
-  `shareswadil` int(11) DEFAULT NULL,
-  `sps` float DEFAULT NULL,
-  `tangibles` int(11) DEFAULT NULL,
-  `taxassets` int(11) DEFAULT NULL,
-  `taxexp` int(11) DEFAULT NULL,
-  `taxliabilities` int(11) DEFAULT NULL,
-  `tbvps` float DEFAULT NULL,
-  `workingcapital` int(11) DEFAULT NULL,
+  `tangibles` bigint(20) DEFAULT NULL,
+  `workingcapital` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`ticker`,`dateCal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Daten Export vom Benutzer nicht ausgewählt
 
--- Dumping structure for table stockdb_akramluna.stockmain
+-- Exportiere Struktur von Tabelle stockdb_akramluna.stockmain
 CREATE TABLE IF NOT EXISTS `stockmain` (
   `ticker` varchar(20) NOT NULL,
   `lastUpdate` datetime DEFAULT NULL,
@@ -130,9 +106,9 @@ CREATE TABLE IF NOT EXISTS `stockmain` (
   PRIMARY KEY (`ticker`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Daten Export vom Benutzer nicht ausgewählt
 
--- Dumping structure for table stockdb_akramluna.stocknews
+-- Exportiere Struktur von Tabelle stockdb_akramluna.stocknews
 CREATE TABLE IF NOT EXISTS `stocknews` (
   `benzingaID` int(11) NOT NULL,
   `ticker` varchar(50) DEFAULT NULL,
@@ -144,9 +120,9 @@ CREATE TABLE IF NOT EXISTS `stocknews` (
   PRIMARY KEY (`benzingaID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Daten Export vom Benutzer nicht ausgewählt
 
--- Dumping structure for table stockdb_akramluna.stockprices
+-- Exportiere Struktur von Tabelle stockdb_akramluna.stockprices
 CREATE TABLE IF NOT EXISTS `stockprices` (
   `ticker` varchar(50) NOT NULL,
   `datePrice` date NOT NULL,
@@ -159,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `stockprices` (
   PRIMARY KEY (`ticker`,`datePrice`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Daten Export vom Benutzer nicht ausgewählt
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
