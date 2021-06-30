2274696b3bf2c9b20d390468525c09d3a0c97e54
montielantonio <ggtonushy@gmail.com>
2021-06-30 08:48:26 +0200

    upd

--- CanadaThornhill/ScrapeCauseiq.py
+++ CanadaThornhill/ScrapeCauseiq.py
@@ -16,28 +16,24 @@ load_dotenv(find_dotenv())
 MAIL = os.environ.get("MAIL")
 MAILPW = os.environ.get("MAILPW")
 
-# link = "https://www.causeiq.com/directory/business-and-community-development-organizations-list/"
+
 link = "https://www.causeiq.com/search/organizations/so_4ef5f36d1e427557#list"
-# link = "https://www.causeiq.com/search/organizations/so_4ef5f36d1e427557/#list"
-# link = "https://www.causeiq.com/search/organizations/so_4ef5f36d1e427557#list"
 
 options = Options()
 # options.add_argument('--headless')
 options.add_experimental_option ('excludeSwitches', ['enable-logging'])
 path = os.path.abspath (os.path.dirname (sys.argv[0]))
-if sys.platform == "win32": cd = '/chromedriver.exe'
-elif sys.platform == "linux": cd = '/chromedriver'
-elif sys.platform == "darwin": cd = '/chromedriver'
+cd = '/chromedriver.exe'
 driver = webdriver.Chrome (path + cd, options=options)
 driver.get (link)
 driver.find_element_by_xpath ('//*[@id="top"]/div/div[2]/a').click ()
-driver.find_element_by_id("id_login").send_keys(MAIL)
-driver.find_element_by_id("id_password").send_keys(MAILPW)
+driver.find_element_by_id("id_login").send_keys("rapid1898@gmail.com")
+driver.find_element_by_id("id_password").send_keys("12345678")
 driver.find_element_by_xpath ('//*[@id="stage_content"]/div/div/div/div[2]/form/button').click ()
 
-element = driver.find_element_by_xpath ('//*[@id="search_filters_new"]/div/div[2]/div[3]/div[5]/div')
-driver.execute_script("arguments[0].setAttribute('class','new-filter-item active')", element)
-driver.find_element_by_xpath ('//*[@id="search_filters_new"]/div/div[2]/div[3]/div[5]/div').click()
+# element = driver.find_element_by_xpath ('//*[@id="search_filters_new"]/div/div[2]/div[3]/div[5]/div')
+# driver.execute_script("arguments[0].setAttribute('class','new-filter-item active')", element)
+# driver.find_element_by_xpath ('//*[@id="search_filters_new"]/div/div[2]/div[3]/div[5]/div').click()
 
 
 
