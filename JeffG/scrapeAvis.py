b23d0ad1a91c82d09a6bd7b403be4c6fbebc5adf
montielantonio <ggtonushy@gmail.com>
2021-07-06 18:38:10 +0200

    upd

--- JeffG/scrapeAvis.py
+++ JeffG/scrapeAvis.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from random import choice
 from selenium import webdriver     
 from sys import platform 
-
+from fake_useragent import UserAgent
 
 if __name__ == '__main__':
   WAIT = 1
@@ -25,18 +25,29 @@ if __name__ == '__main__':
   elif platform == "linux": cd = '/chromedriver'
   elif platform == "darwin": cd = '/chromedriver'
   options = Options()
-  options.add_argument('--headless')
-  options.add_experimental_option ('excludeSwitches', ['enable-logging'])            
-  options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36")    
+  ua = UserAgent()
+  userAgent = ua.random
+  # options.add_argument('--headless')
+  options.add_argument("--window-size=1920x1080")
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+  options.add_experimental_option ('excludeSwitches', ['enable-logging'])    
+  options.add_argument(f'user-agent={userAgent}')        
 
   headers = {
-    # 'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'
   }
 
   driver = webdriver.Chrome (path + cd, options=options)
   driver.get (link)
   time.sleep(WAIT)
+
+  try:
+    driver.find_element_by_css_selector("span[aria-label='Close']").click()
+    # driver.find_element_by_xpath('//*[@id="resWidgetContainer"]/div/div[2]/div[12]/div[2]/div/div/div/div/div/div[1]/h3/span').click()    
+  except:
+    pass
+
   print(f"Read location VIE...")
   driver.find_element_by_id("PicLoc_value").send_keys("VIE") 
   time.sleep(WAIT)
