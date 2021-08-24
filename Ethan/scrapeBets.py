c656e4d065139722ebf153716b2f81d98d0ee760
montielantonio <ggtonushy@gmail.com>
2021-08-24 22:18:05 +0200

    upd

--- Ethan/scrapeBets.py
+++ Ethan/scrapeBets.py
@@ -8,10 +8,14 @@ from sys import platform
 import os, sys
 import xlwings as xw
 from datetime import datetime, timedelta
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 if __name__ == '__main__':
   SAVE_INTERVAL = 5
-  WAIT = 0.5
+  WAIT = 3
   FN = "ScrapeBets.xlsx"
   path = os.path.abspath(os.path.dirname(sys.argv[0]))  
   HEADERS = {
@@ -33,16 +37,11 @@ if __name__ == '__main__':
           nextRow = int(idx+3)
           break
   
+  ### sportbet.com ###
   link = "https://www.sportsbet.com.au/racing-schedule/horse-racing"
   page = requests.get (link, headers=HEADERS)
   soup = BeautifulSoup (page.content, "html.parser")
   time.sleep(WAIT)
-
-  # tmpTRs = soup.find_all("tr")
-  # for i,e in enumerate(tmpTRs):
-  #   print(f"\n{i}:")
-  #   print(e.prettify())
-  # print(len(tmpTRs))
 
   ergLinks = []
   tmpHREFs = soup.find_all("a")
@@ -101,16 +100,15 @@ if __name__ == '__main__':
     if len(selection) > 2:
       ws["G" + str (nextRow)].value = selection[2]
     if len(selection) > 3:
-      ws["H" + str (nextRow)].value = selection[2]
-    if len(selection) > 4:
       ws["H" + str (nextRow)].value = selection[3]
-    if len(selection) > 5:
+    if len(selection) > 4:
       ws["H" + str (nextRow)].value = selection[4]
+    if len(selection) > 5:
+      ws["H" + str (nextRow)].value = selection[5]
     
     driver.quit()
 
     nextRow += 1
-
     if nextRow % SAVE_INTERVAL == 0:
         wb.save (fn)
         # wb.close()
@@ -118,18 +116,124 @@ if __name__ == '__main__':
 
 
 
+  ## betfair.com ###
+  # link = "https://www.betfair.com.au/exchange/plus/horse-racing"
+
+  # options = Options()
+  # # options.add_argument('--headless')
+  # options.add_experimental_option ('excludeSwitches', ['enable-logging'])
+  # options.add_argument("start-maximized")
+  # path = os.path.abspath (os.path.dirname (sys.argv[0]))
+
+  # if platform == "win32": cd = '/chromedriver.exe'
+  # elif platform == "linux": cd = '/chromedriver'
+  # elif platform == "darwin": cd = '/chromedriver'
+  # driver = webdriver.Chrome (path + cd, options=options)
+  # driver.get (link)  # Read link
+  # time.sleep (WAIT)  # Wait till the full site is loaded
+  # # driver.find_element_by_link_text('"AUS "').click()
+  # # driver.find_element_by_partial_link_text('AUS').click()
+
+  # wait = WebDriverWait(driver, 10)
+  # tmpElem = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@link-id="309627"]')))
+  # tmpElem.click() 
+
+  # soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser
 
 
 
+  ### ladbrokes.com ###
+  link = "https://www.ladbrokes.com.au/racing"
+  today = datetime.today().date()
+  tomorrow = today + timedelta(days=1)
+  link = f"{link}?date={tomorrow}"
+  print(f"Working for: {link}")
 
+  options = Options()
+  options.add_argument('--headless')
+  options.add_experimental_option ('excludeSwitches', ['enable-logging'])
+  options.add_argument("start-maximized")
+  options.add_argument('window-size=1920x1080')								  
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')  
+  path = os.path.abspath (os.path.dirname (sys.argv[0]))
+  if platform == "win32": cd = '/chromedriver.exe'
+  elif platform == "linux": cd = '/chromedriver'
+  elif platform == "darwin": cd = '/chromedriver'
+  driver = webdriver.Chrome (path + cd, options=options)
+  driver.get (link)  # Read link
+  time.sleep (WAIT)  # Wait till the full site is loaded
+  soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
+  time.sleep (WAIT)  # Wait till the full site is loaded
 
+  raceLinks = []
+  horseRaceDomestic = False
+  hrefLinks = soup.find_all("a")
+  
+  # for e in hrefLinks:
+  #   print(e.get("href"))
+  # exit()
 
+  for e in hrefLinks:
+    e = e.get("href")
+    if e == "/horse-racing/domestic":
+      horseRaceDomestic = True
+      continue
+    if e == "/horse-racing/international":
+      break
+    if horseRaceDomestic and e not in raceLinks:
+      raceLinks.append("https://www.ladbrokes.com.au" + e)
+      # break
+  driver.quit()  
 
+  raceCount = {}
+  for eLink in raceLinks:
+    print(f"Working for race {eLink}...")
+    driver = webdriver.Chrome (path + cd, options=options)
+    driver.get (eLink)  # Read link
+    time.sleep (WAIT)  # Wait till the full site is loaded
+    soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
+    time.sleep (WAIT)  # Wait till the full site is loaded    
 
+    tmpTrack = eLink.split("https://www.ladbrokes.com.au/racing/")[1]
+    track = tmpTrack.split("/")[0].capitalize()
 
+    if track not in raceCount:
+      raceCount[track] = 1
+      race = 1
+    else:
+      raceCount[track] += 1
+      race = raceCount[track]
 
+    selection = []
+    tmpDIVs = soup.find_all("div", class_="saddle-number")
+    for e in tmpDIVs:
+      selection.append(int(e.text))
 
+    # print(f"DEBUG - {track, race, selection}")
+    print(f"Write to excel - {track, race, selection}...")
 
+    ws["A" + str (nextRow)].value = "Ladbrokes"
+    ws["B" + str (nextRow)].value = datetime.today ()
+    ws["C" + str (nextRow)].value = track
+    ws["D" + str (nextRow)].value = race
+    if len(selection) > 0:
+      ws["E" + str (nextRow)].value = selection[0]
+    if len(selection) > 1:
+      ws["F" + str (nextRow)].value = selection[1]
+    if len(selection) > 2:
+      ws["G" + str (nextRow)].value = selection[2]
+    if len(selection) > 3:
+      ws["H" + str (nextRow)].value = selection[3]
+    if len(selection) > 4:
+      ws["H" + str (nextRow)].value = selection[4]
+    if len(selection) > 5:
+      ws["H" + str (nextRow)].value = selection[5]
 
+    driver.quit()  
 
-
+    nextRow += 1
+    if nextRow % SAVE_INTERVAL == 0:
+        wb.save (fn)
+        # wb.close()
+        print ("Saved to disk...")
