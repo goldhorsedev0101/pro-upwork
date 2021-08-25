7cbed0847006d3a178b87068d06a0e0ab10c9390
montielantonio <ggtonushy@gmail.com>
2021-08-25 20:43:13 +0200

    upd

--- Ethan/scrapeBets.py
+++ Ethan/scrapeBets.py
@@ -38,81 +38,81 @@ if __name__ == '__main__':
           break
   
   ### sportbet.com ###
-  link = "https://www.sportsbet.com.au/racing-schedule/horse-racing"
-  page = requests.get (link, headers=HEADERS)
-  soup = BeautifulSoup (page.content, "html.parser")
-  time.sleep(WAIT)
+  # link = "https://www.sportsbet.com.au/racing-schedule/horse-racing"
+  # page = requests.get (link, headers=HEADERS)
+  # soup = BeautifulSoup (page.content, "html.parser")
+  # time.sleep(WAIT)
 
-  ergLinks = []
-  tmpHREFs = soup.find_all("a")
-  for i,e in enumerate(tmpHREFs):
-    e = e.get("href")
-    if "horse-racing" in e and "australia-nz" in e:
-      ergLinks.append("https://www.sportsbet.com.au" + e)
+  # ergLinks = []
+  # tmpHREFs = soup.find_all("a")
+  # for i,e in enumerate(tmpHREFs):
+  #   e = e.get("href")
+  #   if "horse-racing" in e and "australia-nz" in e:
+  #     ergLinks.append("https://www.sportsbet.com.au" + e)
 
-  ergDetailLinks = []
-  for eLinks in ergLinks:
-    print(f"Working on place {eLinks}...")
-    page = requests.get (eLinks, headers=HEADERS)
-    soup = BeautifulSoup (page.content, "html.parser")
-    time.sleep(WAIT)
-    tmpHREFs = soup.find_all("a")
-    for i,e in enumerate(tmpHREFs):
-      e = e.get("href")
-      if "horse-racing" in e and "australia-nz" in e:
-        ergDetailLinks.append("https://www.sportsbet.com.au" + e)
-    # break
+  # ergDetailLinks = []
+  # for eLinks in ergLinks:
+  #   print(f"Working on place {eLinks}...")
+  #   page = requests.get (eLinks, headers=HEADERS)
+  #   soup = BeautifulSoup (page.content, "html.parser")
+  #   time.sleep(WAIT)
+  #   tmpHREFs = soup.find_all("a")
+  #   for i,e in enumerate(tmpHREFs):
+  #     e = e.get("href")
+  #     if "horse-racing" in e and "australia-nz" in e:
+  #       ergDetailLinks.append("https://www.sportsbet.com.au" + e)
+  #   # break
 
-  options = Options()
-  options.add_argument('--headless')
-  options.add_experimental_option ('excludeSwitches', ['enable-logging'])
-  options.add_argument("start-maximized")
-  path = os.path.abspath (os.path.dirname (sys.argv[0]))
+  # options = Options()
+  # options.add_argument('--headless')
+  # options.add_experimental_option ('excludeSwitches', ['enable-logging'])
+  # options.add_argument("start-maximized")
+  # path = os.path.abspath (os.path.dirname (sys.argv[0]))
 
-  for eLink in ergDetailLinks:
-    print(f"Working on race {eLink}...")
-    tmpTrack = eLink.split("/")
-    track = tmpTrack[5].capitalize()
-    race = tmpTrack[6].split("-")[1]
+  # for eLink in ergDetailLinks:
+  #   print(f"Working on race {eLink}...")
+  #   tmpTrack = eLink.split("/")
+  #   track = tmpTrack[5].capitalize()
+  #   race = tmpTrack[6].split("-")[1]
   
-    if platform == "win32": cd = '/chromedriver.exe'
-    elif platform == "linux": cd = '/chromedriver'
-    elif platform == "darwin": cd = '/chromedriver'
-    driver = webdriver.Chrome (path + cd, options=options)
-    driver.get (eLink)  # Read link
-    time.sleep (WAIT)  # Wait till the full site is loaded
-    soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser
-    time.sleep (WAIT)  
-    selection = soup.find("span", {"data-automation-id": "expert-tips-list"})
-    selection = list(selection.text.split(" ")[1].split("-"))
-    selection = list(map(int,selection))
-    # print(f"DEBUG - {track, race, selection}")
-    print(f"Write to excel - {track, race, selection}...")
+  #   if platform == "win32": cd = '/chromedriver.exe'
+  #   elif platform == "linux": cd = '/chromedriver'
+  #   elif platform == "darwin": cd = '/chromedriver'
+  #   driver = webdriver.Chrome (path + cd, options=options)
+  #   driver.get (eLink)  # Read link
+  #   time.sleep (WAIT)  # Wait till the full site is loaded
+  #   soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser
+  #   time.sleep (WAIT)  
+  #   selection = soup.find("span", {"data-automation-id": "expert-tips-list"})
+  #   selection = list(selection.text.split(" ")[1].split("-"))
+  #   selection = list(map(int,selection))
+  #   # print(f"DEBUG - {track, race, selection}")
+  #   print(f"Write to excel - {track, race, selection}...")
 
-    ws["A" + str (nextRow)].value = "Sportsbet"
-    ws["B" + str (nextRow)].value = datetime.today ()
-    ws["C" + str (nextRow)].value = track
-    ws["D" + str (nextRow)].value = race
-    if len(selection) > 0:
-      ws["E" + str (nextRow)].value = selection[0]
-    if len(selection) > 1:
-      ws["F" + str (nextRow)].value = selection[1]
-    if len(selection) > 2:
-      ws["G" + str (nextRow)].value = selection[2]
-    if len(selection) > 3:
-      ws["H" + str (nextRow)].value = selection[3]
-    if len(selection) > 4:
-      ws["H" + str (nextRow)].value = selection[4]
-    if len(selection) > 5:
-      ws["H" + str (nextRow)].value = selection[5]
+  #   ws["A" + str (nextRow)].value = "Sportsbet"
+  #   ws["B" + str (nextRow)].value = datetime.today ()
+  #   ws["C" + str (nextRow)].value = track
+  #   ws["D" + str (nextRow)].value = race
+  #   if len(selection) > 0:
+  #     ws["E" + str (nextRow)].value = selection[0]
+  #   if len(selection) > 1:
+  #     ws["F" + str (nextRow)].value = selection[1]
+  #   if len(selection) > 2:
+  #     ws["G" + str (nextRow)].value = selection[2]
+  #   if len(selection) > 3:
+  #     ws["H" + str (nextRow)].value = selection[3]
+  #   if len(selection) > 4:
+  #     ws["H" + str (nextRow)].value = selection[4]
+  #   if len(selection) > 5:
+  #     ws["H" + str (nextRow)].value = selection[5]
     
-    driver.quit()
+  #   driver.quit()
 
-    nextRow += 1
-    if nextRow % SAVE_INTERVAL == 0:
-        wb.save (fn)
-        # wb.close()
-        print ("Saved to disk...")
+  #   nextRow += 1
+  #   if nextRow % SAVE_INTERVAL == 0:
+  #       wb.save (fn)
+  #       # wb.close()
+  #       print ("Saved to disk...")
 
 
 
@@ -143,10 +143,106 @@ if __name__ == '__main__':
 
 
   ### ladbrokes.com ###
-  link = "https://www.ladbrokes.com.au/racing"
-  today = datetime.today().date()
-  tomorrow = today + timedelta(days=1)
-  link = f"{link}?date={tomorrow}"
+  # link = "https://www.ladbrokes.com.au/racing"
+  # today = datetime.today().date()
+  # tomorrow = today + timedelta(days=1)
+  # link = f"{link}?date={tomorrow}"
+  # print(f"Working for: {link}")
+
+  # options = Options()
+  # options.add_argument('--headless')
+  # options.add_experimental_option ('excludeSwitches', ['enable-logging'])
+  # options.add_argument("start-maximized")
+  # options.add_argument('window-size=1920x1080')								  
+  # options.add_argument('--no-sandbox')
+  # options.add_argument('--disable-gpu')  
+  # path = os.path.abspath (os.path.dirname (sys.argv[0]))
+  # if platform == "win32": cd = '/chromedriver.exe'
+  # elif platform == "linux": cd = '/chromedriver'
+  # elif platform == "darwin": cd = '/chromedriver'
+  # driver = webdriver.Chrome (path + cd, options=options)
+  # driver.get (link)  # Read link
+  # time.sleep (WAIT)  # Wait till the full site is loaded
+  # soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
+  # time.sleep (WAIT)  # Wait till the full site is loaded
+
+  # raceLinks = []
+  # horseRaceDomestic = False
+  # hrefLinks = soup.find_all("a")
+  
+  # # for e in hrefLinks:
+  # #   print(e.get("href"))
+  # # exit()
+
+  # for e in hrefLinks:
+  #   e = e.get("href")
+  #   if e == "/horse-racing/domestic":
+  #     horseRaceDomestic = True
+  #     continue
+  #   if e == "/horse-racing/international":
+  #     break
+  #   if horseRaceDomestic and e not in raceLinks:
+  #     raceLinks.append("https://www.ladbrokes.com.au" + e)
+  #     # break
+  # driver.quit()  
+
+  # raceCount = {}
+  # for eLink in raceLinks:
+  #   print(f"Working for race {eLink}...")
+  #   driver = webdriver.Chrome (path + cd, options=options)
+  #   driver.get (eLink)  # Read link
+  #   time.sleep (WAIT)  # Wait till the full site is loaded
+  #   soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
+  #   time.sleep (WAIT)  # Wait till the full site is loaded    
+
+  #   tmpTrack = eLink.split("https://www.ladbrokes.com.au/racing/")[1]
+  #   track = tmpTrack.split("/")[0].capitalize()
+
+  #   if track not in raceCount:
+  #     raceCount[track] = 1
+  #     race = 1
+  #   else:
+  #     raceCount[track] += 1
+  #     race = raceCount[track]
+
+  #   selection = []
+  #   tmpDIVs = soup.find_all("div", class_="saddle-number")
+  #   for e in tmpDIVs:
+  #     selection.append(int(e.text))
+
+  #   # print(f"DEBUG - {track, race, selection}")
+  #   print(f"Write to excel - {track, race, selection}...")
+
+  #   ws["A" + str (nextRow)].value = "Ladbrokes"
+  #   ws["B" + str (nextRow)].value = datetime.today ()
+  #   ws["C" + str (nextRow)].value = track
+  #   ws["D" + str (nextRow)].value = race
+  #   if len(selection) > 0:
+  #     ws["E" + str (nextRow)].value = selection[0]
+  #   if len(selection) > 1:
+  #     ws["F" + str (nextRow)].value = selection[1]
+  #   if len(selection) > 2:
+  #     ws["G" + str (nextRow)].value = selection[2]
+  #   if len(selection) > 3:
+  #     ws["H" + str (nextRow)].value = selection[3]
+  #   if len(selection) > 4:
+  #     ws["H" + str (nextRow)].value = selection[4]
+  #   if len(selection) > 5:
+  #     ws["H" + str (nextRow)].value = selection[5]
+
+  #   driver.quit()  
+
+  #   nextRow += 1
+  #   if nextRow % SAVE_INTERVAL == 0:
+  #       wb.save (fn)
+  #       # wb.close()
+  #       print ("Saved to disk...")
+
+
+
+  ### ladbrokes.com ###
+  WAIT = 1
+  link = "https://www.tab.com.au/racing/meetings/today/R"  
   print(f"Working for: {link}")
 
   options = Options()
@@ -161,79 +257,77 @@ if __name__ == '__main__':
   elif platform == "linux": cd = '/chromedriver'
   elif platform == "darwin": cd = '/chromedriver'
   driver = webdriver.Chrome (path + cd, options=options)
-  driver.get (link)  # Read link
-  time.sleep (WAIT)  # Wait till the full site is loaded
-  soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
-  time.sleep (WAIT)  # Wait till the full site is loaded
-
-  raceLinks = []
-  horseRaceDomestic = False
-  hrefLinks = soup.find_all("a")
+  driver.get (link)
+  time.sleep (WAIT)
+  driver.find_element(By.XPATH, '//button[text()="Select"]').click()
+  driver.get ("https://www.tab.com.au/")
+  time.sleep (WAIT)
+  driver.back()
+  time.sleep (WAIT)    
+  soup = BeautifulSoup (driver.page_source, 'html.parser')
+  time.sleep (WAIT)
   
-  # for e in hrefLinks:
-  #   print(e.get("href"))
-  # exit()
+  tmptrackList = []
+  trackList = []
+  tmpDIVs = soup.find_all("div", {"class": "_e4e14m"})
+  for e in tmpDIVs:
+    tmpSPANs = e.find_all("span")
+    for e2 in tmpSPANs:
+      tmptrackList.append(e2.text)
+  for i,e in enumerate(tmptrackList):
+    if i % 2 == 0:
+      if tmptrackList[i+1] in ["(GBR)","(IRL)","(JPN)","(CAN)","(FRA)","(SWE)","(BRA)"]:
+        break
+      else:
+        trackList.append(e)
 
-  for e in hrefLinks:
-    e = e.get("href")
-    if e == "/horse-racing/domestic":
-      horseRaceDomestic = True
-      continue
-    if e == "/horse-racing/international":
-      break
-    if horseRaceDomestic and e not in raceLinks:
-      raceLinks.append("https://www.ladbrokes.com.au" + e)
-      # break
-  driver.quit()  
+  tmpDIVs = soup.find("div", {"class": "_1k65463"})
+  tmpDIVs2 = tmpDIVs.find_all("div", {"class": "_1xxbwka"})
+  # print(len(tmpDIVs2))
 
-  raceCount = {}
-  for eLink in raceLinks:
-    print(f"Working for race {eLink}...")
-    driver = webdriver.Chrome (path + cd, options=options)
-    driver.get (eLink)  # Read link
-    time.sleep (WAIT)  # Wait till the full site is loaded
-    soup = BeautifulSoup (driver.page_source, 'html.parser')  # Read page with html.parser  
-    time.sleep (WAIT)  # Wait till the full site is loaded    
+  countRaces = 0
+  tmpList = []
+  for i,e in enumerate(tmpDIVs2):
+    tmpSPANs = e.find_all("span")
+    for i2,e2 in enumerate(tmpSPANs):
+      e2 = e2.text
+      if ":" in e2:
+        continue    
+      if e2 == "R1":
+        countRaces +=1
+      if countRaces > len(trackList):
+        break         
+      if len(e2) == 2:
+        tmpList.append(trackList[countRaces - 1])        
+        tmpList.append(e2.replace("R",""))
+      else:
+        selection = e2.split(" ")
+        tmpList.append(selection)
 
-    tmpTrack = eLink.split("https://www.ladbrokes.com.au/racing/")[1]
-    track = tmpTrack.split("/")[0].capitalize()
+  for i,e in enumerate(tmpList):
+    if i % 3 == 0:
+      print(f"Working on {tmpList[i]}, Race {tmpList[i + 1]}, Selection {tmpList[i + 2]}...")        
+      
+      ws["A" + str (nextRow)].value = "TAB"
+      ws["B" + str (nextRow)].value = datetime.today ()
+      ws["C" + str (nextRow)].value = tmpList[i]
+      ws["D" + str (nextRow)].value = tmpList[i + 1]
+      selection = tmpList[i + 2]
+      if len(selection) > 0:
+        ws["E" + str (nextRow)].value = selection[0]
+      if len(selection) > 1:
+        ws["F" + str (nextRow)].value = selection[1]
+      if len(selection) > 2:
+        ws["G" + str (nextRow)].value = selection[2]
+      if len(selection) > 3:
+        ws["H" + str (nextRow)].value = selection[3]
+      if len(selection) > 4:
+        ws["H" + str (nextRow)].value = selection[4]
+      if len(selection) > 5:
+        ws["H" + str (nextRow)].value = selection[5]
 
-    if track not in raceCount:
-      raceCount[track] = 1
-      race = 1
-    else:
-      raceCount[track] += 1
-      race = raceCount[track]
-
-    selection = []
-    tmpDIVs = soup.find_all("div", class_="saddle-number")
-    for e in tmpDIVs:
-      selection.append(int(e.text))
-
-    # print(f"DEBUG - {track, race, selection}")
-    print(f"Write to excel - {track, race, selection}...")
-
-    ws["A" + str (nextRow)].value = "Ladbrokes"
-    ws["B" + str (nextRow)].value = datetime.today ()
-    ws["C" + str (nextRow)].value = track
-    ws["D" + str (nextRow)].value = race
-    if len(selection) > 0:
-      ws["E" + str (nextRow)].value = selection[0]
-    if len(selection) > 1:
-      ws["F" + str (nextRow)].value = selection[1]
-    if len(selection) > 2:
-      ws["G" + str (nextRow)].value = selection[2]
-    if len(selection) > 3:
-      ws["H" + str (nextRow)].value = selection[3]
-    if len(selection) > 4:
-      ws["H" + str (nextRow)].value = selection[4]
-    if len(selection) > 5:
-      ws["H" + str (nextRow)].value = selection[5]
-
-    driver.quit()  
-
-    nextRow += 1
-    if nextRow % SAVE_INTERVAL == 0:
-        wb.save (fn)
-        # wb.close()
-        print ("Saved to disk...")
+      nextRow += 1
+      if nextRow % SAVE_INTERVAL == 0:
+          wb.save (fn)
+          # wb.close()
+          print ("Saved to disk...")
